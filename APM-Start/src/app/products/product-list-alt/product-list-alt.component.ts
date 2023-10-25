d7f2342885346d3a98772bb80cef19c3a3351266
Juan Vargas Salamanca <juan.salamanca@nessy.com>
2023-10-25 15:25:52 +0100

    je: changes made up until chapter 8 of rxjs deborah

--- APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,34 +1,36 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-
-import { Subscription } from 'rxjs';
-
+import { Observable, Subscription } from 'rxjs';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
-  templateUrl: './product-list-alt.component.html'
+  templateUrl: './product-list-alt.component.html',
 })
-export class ProductListAltComponent implements OnInit, OnDestroy {
+export class ProductListAltComponent {
   pageTitle = 'Products';
   errorMessage = '';
   selectedProductId = 0;
 
-  products: Product[] = [];
+  // products: Product[] = [];
   sub!: Subscription;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
-    this.sub = this.productService.getProducts().subscribe({
-      next: products => this.products = products,
-      error: err => this.errorMessage = err
-    });
-  }
+  products = this.productService.products$;
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  //- the below i think is no longer needed
+
+  // ngOnInit(): void {
+  //   this.sub = this.productService.getProducts().subscribe({
+  //     next: (products) => (this.products = products),
+  //     error: (err: string) => (this.errorMessage = err),
+  //   });
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
 
   onSelected(productId: number): void {
     console.log('Not yet implemented');
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { EMPTY, Observable, Subscription, catchError, filter, map } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
 
 import { Product } from './product';
@@ -8,29 +8,47 @@ import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
+  selectedCategoryId = 1;
 
-  products: Product[] = [];
-  sub!: Subscription;
+  // products: Observable<Product[]> | undefined;
+  // sub!: Subscription;
 
-  constructor(private productService: ProductService) { }
+  products$ = this.productService.productWithCategories$.pipe(
+    catchError((err) => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
+
+  productsSimpleFilter$ = this.productService.productWithCategories$.pipe(
+    map((products) =>
+      products.filter((product) =>
+        this.selectedCategoryId
+          ? product.categoryId === this.selectedCategoryId
+          : true
+      )
+    )
+  );
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe({
-        next: products => this.products = products,
-        error: err => this.errorMessage = err
-      });
+    // this.sub = this.productService.getProducts().subscribe({
+    //   next: (products) => (this.products = products),
+    //   error: (err) => (this.errorMessage = err),
+    // });
+    this.products$.subscribe((x) => console.log('prod & cat: ', x));
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
 
   onAdd(): void {
     console.log('Not yet implemented');
